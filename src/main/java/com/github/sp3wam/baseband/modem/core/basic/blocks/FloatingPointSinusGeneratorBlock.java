0f8606f0d5c41a8e23d4b86012920bfc4cede1a6
Witold Markowski <witold.a.markowski@gmail.com>
2025-02-17 22:29:52 +0000

    Add possibility to have sinus generator with DC offseet.

--- src/main/java/com/github/sp3wam/baseband/modem/core/basic/blocks/FloatingPointSinusGeneratorBlock.java
+++ src/main/java/com/github/sp3wam/baseband/modem/core/basic/blocks/FloatingPointSinusGeneratorBlock.java
@@ -8,19 +8,27 @@ import com.github.sp3wam.baseband.modem.core.basic.signals.FloatingPointSignal;
 public class FloatingPointSinusGeneratorBlock extends AbstractBlock< DummySignal, FloatingPointSignal >
 {
     private double amplitude;
+    private double dcOffset;
     private double frequencyHz;
 
     public FloatingPointSinusGeneratorBlock( double amplitude, double frequencyHz )
     {
         this.amplitude = amplitude;
         this.frequencyHz = frequencyHz;
+        this.dcOffset = 0.0;
+    }
+
+    public void setDCOffset( double dcOffset )
+    {
+        this.dcOffset = dcOffset;
     }
 
     @Override
     protected boolean execute0( SystemClock systemClock, DummySignal inputSignalValue )
     {
         double value =
-            amplitude * Math.sin( 2 * Math.PI * frequencyHz * systemClock.getClockValueInSeconds() );
+            amplitude * Math.sin( 2 * Math.PI * frequencyHz * systemClock.getClockValueInSeconds() )
+                + dcOffset;
 
         currentValue = new FloatingPointSignal( value );
 
