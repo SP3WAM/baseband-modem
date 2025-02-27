772fe82a27be0878c6ba59fd4fa5b85971d51b19
Witold Markowski <witold.a.markowski@gmail.com>
2025-02-27 22:04:24 +0000

    Averaging of output bits makes it a bit better.

--- src/main/java/com/github/sp3wam/baseband/modem/impl/morse/detector/AbstractMorseSignalDetectorBlock.java
+++ src/main/java/com/github/sp3wam/baseband/modem/impl/morse/detector/AbstractMorseSignalDetectorBlock.java
@@ -86,7 +86,7 @@ abstract class AbstractMorseSignalDetectorBlock extends AbstractBlock< FloatingP
         fftBlock = new FFTBlock( fftSampleFreq, fftWindowSize );
         fftSlowerSampler = new SamplerBlock< FFTSignal, FFTSignal >( bitSamplerDivider );
         internalBlock = new InternalBlock();
-        bitAveragerBlock = new BitAveragerBlock( 5 );
+        bitAveragerBlock = new BitAveragerBlock( 10 );
 
         // connect the blocks
         floatingPointAveragerBlock.setNextBlock( avgMagnitude );
