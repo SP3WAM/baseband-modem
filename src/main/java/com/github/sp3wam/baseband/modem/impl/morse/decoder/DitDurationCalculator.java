5ebb426f41cdeb0552cc93643c172cffe95af793
Witold Markowski <witold.a.markowski@gmail.com>
2025-03-05 21:35:15 +0000

    Rework dit duration calculator.

--- src/main/java/com/github/sp3wam/baseband/modem/impl/morse/decoder/DitDurationCalculator.java
+++ src/main/java/com/github/sp3wam/baseband/modem/impl/morse/decoder/DitDurationCalculator.java
@@ -18,7 +18,6 @@ class DitDurationCalculator
     private final static int SEGMENTS_MAX_SIZE = 8;
     private List< MorseSegment > segments = new ArrayList<>();
     private Integer ditDuration;
-    private MorseSegment lastAddedSegment = null;
 
     public void addSegment( MorseSegment segment )
     {
@@ -28,19 +27,7 @@ class DitDurationCalculator
             return;
         }
 
-        if( segments.size() > 0 )
-        {
-            MorseSegmentType lastSegmentType = lastAddedSegment.getType();
-            if( lastSegmentType.equals( segment.getType() ) )
-            {
-                throw new IllegalArgumentException( String.format(
-                    "Can't add segment of type %s because the last segment is of the same type.",
-                    segment.getType() ) );
-            }
-        }
-
         segments.add( segment );
-        lastAddedSegment = segment;
 
         if( segments.size() > SEGMENTS_MAX_SIZE )
         {
@@ -67,178 +54,101 @@ class DitDurationCalculator
             return;
         }
 
-        // 1. first pass: calculate coarse dit duration based on Signal segments only
-        Double ditCoarseDuration = calculateDitCoarseDuration();
-
-        if( ditCoarseDuration == null )
+        int minSignalDuration = Integer.MAX_VALUE;
+        int maxSignalDuration = 0;
+        int signalSumm = 0;
+        int signalCount = 0;
+        int silenceSumm = 0;
+        int silenceCount = 0;
+        MorseSegment lastSegment = segments.get( segments.size() - 1 );
+        for( MorseSegment segment : segments )
         {
-            return;
-        }
-
-        // 2. second pass: remove too long silences
-        double maxSilenceDuration = 1.5 * ditCoarseDuration;
-        for( int q = 0; q < segments.size(); q++ )
-        {
-            if( MorseSegmentType.Signal.equals( segments.get( q ).getType() ) )
+            if( segment.getType().equals( MorseSegmentType.Silence ) )
             {
+                if( segment == lastSegment )
+                {
+                    // don't count silence at end
+                    continue;
+                }
+
+                silenceCount++;
+                silenceSumm += segment.getDuration();
+
                 continue;
             }
-
-            if( segments.get( q ).getDuration() > maxSilenceDuration )
+            if( segment.getDuration() > maxSignalDuration )
             {
-                segments.remove( q );
-                q--;
+                maxSignalDuration = segment.getDuration();
             }
+            if( segment.getDuration() < minSignalDuration )
+            {
+                minSignalDuration = segment.getDuration();
+            }
+            signalCount++;
+            signalSumm += segment.getDuration();
         }
 
-        // 3. we need to have at least one Silence available
-        if( segments.size() < 3 )
+        if( signalCount < 2 )
         {
-            // at least Signal, Silence, Signal expected
+            // too few signals, can't calculate dit duration
             return;
         }
 
-        boolean silencePresent = false;
-        for( int q = 1; q < segments.size() - 1; q++ )
+        double avgSignalDuration = ((double)signalSumm) / ((double)signalCount);
+
+        if( maxSignalDuration / minSignalDuration >= 1.5 )
         {
-            if( segments.get( q ).getType().equals( MorseSegmentType.Silence ) )
+            // there are DITs and DAHs
+            double ditSumm = 0.0;
+            int ditCount = 0;
+            for( MorseSegment segment : segments )
             {
-                silencePresent = true;
-                break;
+                if( segment.getType().equals( MorseSegmentType.Silence ) )
+                {
+                    continue;
+                }
+
+                if( segment.getDuration() < avgSignalDuration )
+                {
+                    // we have DIT
+                    ditSumm += segment.getDuration();
+                }
+                else
+                {
+                    // we have DAH
+                    ditSumm += (segment.getDuration() / 3.0);
+                }
+
+                ditCount++;
             }
-        }
-        if( !silencePresent )
-        {
+
+            ditDuration = (int)(ditSumm / ditCount + 0.5);
+
+            LOGGER.debug( String.format( "Calculated DIT duration is %s ticks", ditDuration ) );
+
             return;
         }
 
-        // 4. third pass: calculate fine dit duration based on Signal segments and short duration Silence
-        // segments
-        ditDuration = (int)(calculateDitFineDuration() + 0.5);
+        // there are either DITs or DAHs
+        if(silenceCount == 0)
+        {
+            // not possible to find out between DITs or DAHs
+            
+            return;
+        }
+        
+        double avgSilenceDuration = ((double)silenceSumm) / ((double)silenceCount);
+        if( avgSignalDuration / avgSilenceDuration >= 1.5 )
+        {
+            // we have DAHs
+            ditDuration = (int)(avgSignalDuration / 3.0 + 0.5);
+        }
+        else
+        {
+            // we have DITs
+            ditDuration = (int)(avgSignalDuration + 0.5);
+        }
 
         LOGGER.debug( String.format( "Calculated DIT duration is %s ticks", ditDuration ) );
     }
-
-    /***
-     * Calculates dit coarse duration based on Signal segments only; because signals must follow the time
-     * restrictions of Morse signal, while silence can be indefinitely long (i.e. between words).
-     * 
-     * @return
-     */
-    private Double calculateDitCoarseDuration()
-    {
-        // 1. first pass: calculate average signal duration
-        double summ = 0;
-        int count = 0;
-
-        for( MorseSegment segment : segments )
-        {
-            if( MorseSegmentType.Silence.equals( segment.getType() ) )
-            {
-                continue;
-            }
-
-            summ += segment.getDuration();
-            count++;
-        }
-
-        if( count == 0 )
-        {
-            return null;
-        }
-
-        double avgSignalDuration = summ / ((double)count);
-
-        // 2. second pass: collect dit and dah statistics
-        double ditSumm = 0;
-        int ditCount = 0;
-        double dahSumm = 0;
-        int dahCount = 0;
-        for( MorseSegment segment : segments )
-        {
-            if( MorseSegmentType.Silence.equals( segment.getType() ) )
-            {
-                continue;
-            }
-
-            if( segment.getDuration() < avgSignalDuration )
-            {
-                // consider this segment as dit
-                ditSumm += segment.getDuration();
-                ditCount++;
-            }
-            else
-            {
-                // consider this segment as dah
-                dahSumm += segment.getDuration();
-                dahCount++;
-            }
-        }
-
-        if( ditCount == 0 && dahCount == 0 )
-        {
-            return null;
-        }
-
-        if( ditCount == 0 || dahCount == 0 )
-        {
-            // unknown actually if we have dits or dahs
-            // do an average
-            double byDitDuration = (ditSumm + dahSumm) / (ditCount + dahCount);
-            double byDahDuration = (ditSumm + dahSumm) / (ditCount + dahCount) / 3;
-
-            return (byDitDuration + byDahDuration) / 2;
-        }
-
-        // 3. calculate dit coarse duration
-        return ditSumm / ((double)ditCount) / 2.0 + dahSumm / ((double)dahCount) / 6.0;
-    }
-
-    private Double calculateDitFineDuration()
-    {
-        // 1. first pass: calculate average signal and silence duration
-        // Assumption: we have only short silences available (with the duration around of dit)
-        double summ = 0;
-        int count = 0;
-
-        for( MorseSegment segment : segments )
-        {
-            summ += segment.getDuration();
-            count++;
-        }
-
-        if( count == 0 )
-        {
-            return null;
-        }
-
-        double avgDitDuration = summ / ((double)count);
-
-        // 2. second pass: collect dit and dah statistics
-        double ditSumm = 0;
-        int ditCount = 0;
-        double dahSumm = 0;
-        int dahCount = 0;
-        for( MorseSegment segment : segments )
-        {
-            if( segment.getDuration() < avgDitDuration )
-            {
-                // consider this segment as dit
-                ditSumm += segment.getDuration();
-                ditCount++;
-            }
-            else
-            {
-                // consider this segment as dah
-                dahSumm += segment.getDuration();
-                dahCount++;
-            }
-        }
-
-        // 3. calculate dit duration
-        double ditDuration = ditSumm / ((double)ditCount) / 2.0 + dahSumm / ((double)dahCount) / 6.0;
-
-        return ditDuration;
-    }
-
 }
--- src/test/java/com/github/sp3wam/baseband/modem/impl/morse/decoder/DitDurationCalculatorTest.java
+++ src/test/java/com/github/sp3wam/baseband/modem/impl/morse/decoder/DitDurationCalculatorTest.java
@@ -34,7 +34,7 @@ public class DitDurationCalculatorTest
     }
 
     @Test
-    public void testGetDitDuration_forSilenceAndSignal()
+    public void testGetDitDuration_forSilenceSignal()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 10 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 20 ) );
@@ -43,7 +43,7 @@ public class DitDurationCalculatorTest
     }
 
     @Test
-    public void testGetDitDuration_forSilenceAndSilence()
+    public void testGetDitDuration_forSilenceSilence()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 10 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 20 ) );
@@ -51,8 +51,8 @@ public class DitDurationCalculatorTest
         assertNull( subject.getDitDuration() );
     }
 
-    @Test( expected = IllegalArgumentException.class )
-    public void testGetDitDuration_forSignalAndSignal()
+    @Test
+    public void testGetDitDuration_forSignalSignal()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 24 ) );
@@ -61,7 +61,7 @@ public class DitDurationCalculatorTest
     }
 
     @Test
-    public void testGetDitDuration_forSignalAndSilence()
+    public void testGetDitDuration_forSignalSilence()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 24 ) );
@@ -69,8 +69,8 @@ public class DitDurationCalculatorTest
         assertNull( subject.getDitDuration() );
     }
 
-    @Test( expected = IllegalArgumentException.class )
-    public void testGetDitDuration_forSignalAndSilenceAndSilence()
+    @Test
+    public void testGetDitDuration_forSignalSilenceSilence()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 7 ) );
@@ -80,7 +80,7 @@ public class DitDurationCalculatorTest
     }
 
     @Test
-    public void testGetDitDuration_forSignalAndSilenceAndSignal_1()
+    public void testGetDitDuration_forSignalSilenceSignal_1()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 12 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
@@ -90,12 +90,56 @@ public class DitDurationCalculatorTest
     }
     
     @Test
-    public void testGetDitDuration_forSignalAndSilenceAndSignal_2()
+    public void testGetDitDuration_forSignalSilenceSignal_2()
     {
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
         subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 24 ) );
 
-        assertEquals( 6, subject.getDitDuration().intValue() );
+        assertEquals( 8, subject.getDitDuration().intValue() );
+    }
+    
+    @Test
+    public void testGetDitDuration_forSignalSilenceSignal_3()
+    {
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 4 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 4 ) );
+
+        assertEquals( 4, subject.getDitDuration().intValue() );
+    }
+    
+    @Test
+    public void testGetDitDuration_forSignalSilenceSignalSilence_1()
+    {
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 12 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 12 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 100 ) );
+
+        assertEquals( 4, subject.getDitDuration().intValue() );
+    }
+    
+    @Test
+    public void testGetDitDuration_forSignalSilenceSignalSilence_2()
+    {
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 24 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 100 ) );
+
+        assertEquals( 8, subject.getDitDuration().intValue() );
+    }
+    
+    @Test
+    public void testGetDitDuration_forSignalSilenceSignalSilence_3()
+    {
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 21 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 4 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Signal, 24 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 100 ) );
+        subject.addSegment( new MorseSegment( MorseSegmentType.Silence, 100 ) );
+
+        assertEquals( 8, subject.getDitDuration().intValue() );
     }
 }
