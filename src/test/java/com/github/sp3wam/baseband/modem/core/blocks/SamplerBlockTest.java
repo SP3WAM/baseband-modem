5eb50744357867222f8e5a18060d227f5f870aeb
Witold Markowski <witold.a.markowski@gmail.com>
2025-03-11 18:35:27 +0100

    Fix sampler bock test

--- src/test/java/com/github/sp3wam/baseband/modem/core/blocks/SamplerBlockTest.java
+++ src/test/java/com/github/sp3wam/baseband/modem/core/blocks/SamplerBlockTest.java
@@ -14,15 +14,22 @@ import com.github.sp3wam.baseband.modem.core.pcm.PcmFromWavFileSignalGeneratorBl
 public class SamplerBlockTest
 {
     private final double SIGNAL_AMPLITUDE = 100.0;
-    private final int FFT_SAMPLE_FREQ = 6400;
+    private final int FFT_REQUIRED_SAMPLE_FREQ = 6400;
 
     @Test
     public void test() throws IOException
     {
-        PcmFromWavFileSignalGeneratorBlock wavSignal = new PcmFromWavFileSignalGeneratorBlock( SIGNAL_AMPLITUDE,
-            "src/main/resources/morse/C_morse_code.wav" );
+        PcmFromWavFileSignalGeneratorBlock wavSignal =
+            new PcmFromWavFileSignalGeneratorBlock( SIGNAL_AMPLITUDE,
+                "src/test/resources/com/github/sp3wam/baseband/modem/impl/morse/C_morse_code.wav" );
+        wavSignal.init();
+
+        int samplerDivider = (int)(wavSignal.getSampleRate() / FFT_REQUIRED_SAMPLE_FREQ);
+
+        assertEquals( 6, samplerDivider );
+
         SamplerBlock< FloatingPointSignal, FloatingPointSignal > fftSampler =
-            new SamplerBlock< FloatingPointSignal, FloatingPointSignal >( FFT_SAMPLE_FREQ );
+            new SamplerBlock< FloatingPointSignal, FloatingPointSignal >( samplerDivider );
 
         wavSignal.setNextBlock( fftSampler );
 
@@ -36,6 +43,6 @@ public class SamplerBlockTest
         }
 
         assertEquals( 53803, systemClock.getClockValue() );
-        assertEquals( 7809, fftSampler.getSamplesTaken() );
+        assertEquals( 8968, fftSampler.getSamplesTaken() );
     }
 }
